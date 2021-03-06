0cbac8f69ceed35080543dc2c9996d8b11a41098
eaarda <elifaarda@gmail.com>
2021-03-06 21:44:02 +0300

    put-del method for cats/

--- animals/urls.py
+++ animals/urls.py
@@ -5,6 +5,7 @@ from animals import views
 
 urlpatterns = [
     path('animals/', views.Animal_Objects.as_view()),
-    path('cat/', views.Cat_Objects.as_view())
+    path('cats/', views.Cat_Objects.as_view()),
+    path('cats/<int:pk>', views.Cat_Detail.as_view()),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)  # generalize urls
--- animals/views.py
+++ animals/views.py
@@ -26,3 +26,23 @@ class Cat_Objects(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Cat_Detail(APIView):
+    def get(self, request, pk, format=None):
+        cat = Cat.objects.get(pk=pk)
+        serializer = CatSerializer(cat)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        cat = Cat.objects.get(pk=pk)
+        serializer = CatSerializer(cat, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, foramt=None):
+        cat = Cat.objects.get(pk=pk)
+        cat.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
