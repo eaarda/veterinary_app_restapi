33b9831e390a2d95f40a75795306b4f29ba8eb47
eaarda <elifaarda@gmail.com>
2021-03-06 15:35:07 +0300

    model fixed

--- animals/models.py
+++ animals/models.py
@@ -4,39 +4,39 @@ from polymorphic.models import PolymorphicModel
 
 class Animal(PolymorphicModel):
     name = models.CharField(max_length=80)
-    birthday = models.DateTimeField(null=True, auto_now=True)
+    birthday = models.DateTimeField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(null=True, auto_now=True)
+    updated_at = models.DateTimeField(null=True)
 
     def __str__(self):
-        return self.name
+        return self.name, self.birthday, self.created_at, self.updated_at
 
 
 class Cat(Animal):
     owner_name = models.CharField(max_length=80)
 
     def __str__(self):
-        return self.owner_name
+        return self.name
 
 
 class Dog(Animal):
     owner_name = models.CharField(max_length=80)
 
     def __str__(self):
-        return self.owner_name
+        return self.name
 
 
 class Cow(Animal):
     farm_name = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.farm_name
+        return self.name
 
 
 class Appointment(models.Model):
     name = models.ForeignKey(
         'Animal', related_name='animals', on_delete=models.CASCADE)
-    appointment_datetime = models.DateTimeField(null=True, auto_now=True)
+    appointment_datetime = models.DateTimeField(null=True)
     description = models.CharField(max_length=500, null=True)
 
     def __str__(self):
