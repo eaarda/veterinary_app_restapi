e753bf44f67c6177416111055b2d9541723b0082
eaarda <elifaarda@gmail.com>
2021-03-06 15:18:59 +0300

    polymorphic model and admin integration done

--- animals/admin.py
+++ animals/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Animal, Cat, Dog, Cow, Appointment
+from polymorphic.admin import PolymorphicParentModelAdmin, PolymorphicChildModelAdmin, PolymorphicChildModelFilter
+
 
 # Register your models here.
+admin.site.register(Appointment)
+
+
+@admin.register(Animal)
+class AnimalAdmin(PolymorphicParentModelAdmin):
+    base_model = Animal
+    child_models = (Cat, Dog, Cow)
+    list_filter = (PolymorphicChildModelFilter,)  # optional.
+
+
+@admin.register(Cat)
+class CatAdmin(PolymorphicChildModelAdmin):
+    base_model = Cat
+
+
+@admin.register(Dog)
+class DogAdmin(PolymorphicChildModelAdmin):
+    base_model = Dog
+
+
+@admin.register(Cow)
+class CowAdmin(PolymorphicChildModelAdmin):
+    base_model = Cow
--- animals/models.py
+++ animals/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from polymorphic.models import PolymorphicModel
 
-# Create your models here.
+
+class Animal(PolymorphicModel):
+    name = models.CharField(max_length=80)
+    birthday = models.DateTimeField(null=True, auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Cat(Animal):
+    owner_name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.owner_name
+
+
+class Dog(Animal):
+    owner_name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.owner_name
+
+
+class Cow(Animal):
+    farm_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.farm_name
+
+
+class Appointment(models.Model):
+    name = models.ForeignKey(
+        'Animal', related_name='animals', on_delete=models.CASCADE)
+    appointment_datetime = models.DateTimeField(null=True, auto_now=True)
+    description = models.CharField(max_length=500, null=True)
+
+    def __str__(self):
+        return self.name
