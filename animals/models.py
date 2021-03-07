3a70ecf8ff987d6b199ce81589d4763f6aa80cc1
eaarda <elifaarda@gmail.com>
2021-03-07 21:32:19 +0300

    update time fixed

--- animals/models.py
+++ animals/models.py
@@ -6,7 +6,7 @@ class Animal(PolymorphicModel):
     name = models.CharField(max_length=80)
     birthday = models.DateTimeField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(null=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.name)
